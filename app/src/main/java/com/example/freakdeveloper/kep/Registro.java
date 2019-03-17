8e9f6962e75adc31e8bcde891786be3d86cf4651
ChelitoSF <Jashurtado2001@gmail.com>
2019-03-17 13:07:26 -0600

    Registro BD[Carreras]

--- app/src/main/java/com/example/freakdeveloper/kep/Registro.java
+++ app/src/main/java/com/example/freakdeveloper/kep/Registro.java
@@ -7,19 +7,25 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.Spinner;
-import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
+import com.example.freakdeveloper.kep.model.Carrera;
+import com.example.freakdeveloper.kep.model.Persona;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 
 public class Registro extends AppCompatActivity {
@@ -30,6 +36,10 @@ public class Registro extends AppCompatActivity {
     private ProgressDialog Dialog;
     private Spinner EscuelaA, EscuelaI;
     private FirebaseAuth.AuthStateListener AuthListener;
+    //PARA FIREBASE
+    private DatabaseReference databaseReference;
+
+    private  static final String nodoPersona="Personas";
 
     @Override
     protected void onStart()
@@ -49,6 +59,10 @@ public class Registro extends AppCompatActivity {
 
         Auth = FirebaseAuth.getInstance();
         Dialog = new ProgressDialog(this);
+
+        //PARA FIREBASE
+        //FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        databaseReference = FirebaseDatabase.getInstance().getReference();
 
         RegistroButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,33 +97,44 @@ public class Registro extends AppCompatActivity {
 
         if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(contra) && !TextUtils.isEmpty(contra2))
         {
-            if (contra2.equals(contra))
-            {
-                Dialog.setMessage("Registrando...");
-                Dialog.show();
-                Auth.createUserWithEmailAndPassword(email, contra).addOnCompleteListener(new OnCompleteListener<AuthResult>()
-                {
+            //anterior
+            if (contra2.equals(contra)) {
+                Task<AuthResult> authResultTask = Auth.createUserWithEmailAndPassword(email, contra)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 Dialog.dismiss();
-                                if (task.isSuccessful())
-                                {
+                                if (task.isSuccessful()) {
                                     Auth.signInWithEmailAndPassword(email, contra);
+                                    //Toast.makeText(ActivityRegister.this, user_id, Toast.LENGTH_SHORT).show();
 
-                                    DatabaseReference Database = FirebaseDatabase.getInstance().getReference().child("Usuarios");
+                                    DatabaseReference Database = FirebaseDatabase.getInstance().getReference().child("usuarios");
                                     DatabaseReference currentUserDB = Database.child(Auth.getCurrentUser().getUid());
-                                    currentUserDB.child("NickName").setValue(name);
-                                    currentUserDB.child("Escuela_Actual").setValue(EA);
-                                    currentUserDB.child("Escuela_Ingreso").setValue(EI);
+                                    //nuevo
+                                    Auth.createUserWithEmailAndPassword(email, contra).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            Dialog.dismiss();
+                                            if (task.isSuccessful()) {
+
+                                                Auth.signInWithEmailAndPassword(email, contra);
+                                                String ID = Auth.getCurrentUser().getUid();
+                                                Persona persona = new Persona(ID, name, EA, EI,email,contra);
+                                                databaseReference.child(nodoPersona).child(persona.getIdPersona()).setValue(persona);
+
+                                            } else {
+                                                Toast.makeText(Registro.this, "Error En El Registro", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+
+                                    Dialog.setMessage("Registrando...");
+                                    Dialog.show();
                                 }
-                                else
-                                {
-                                    Toast.makeText(Registro.this, "Error En El Registro", Toast.LENGTH_SHORT).show();
-                                }
+
                             }
                         });
             }
-
         }
     }
 
@@ -117,18 +142,48 @@ public class Registro extends AppCompatActivity {
     {
         EscuelaA = (Spinner) findViewById(R.id.EActual);
         String[] vocacional = {"Escuela Actual", "CECyT 1", "CECyT 2", "CECyT 3", "CECyT 4", "CECyT 5", "CECyT 6", "CECyT 7", "CECyT 8", "CECyT 9",
-                "CECyT 10", "CECyT 11", "CECyT 12", "CECyT 13", "CECyT 14", "CECyT 15", "CECyT 16", "CECyT 17", "CECyT 18", "CET"};
-        EscuelaA.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, vocacional));
+                "CECyT 10", "CECyT 11", "CECyT 12", "CECyT 13", "CECyT 14", "CECyT 15", "CECyT 16", "CECyT 17", "CECyT 18", "CET","ENP1","ENP2"
+                ,"ENP3","ENP4","ENP5","ENP6","ENP7","ENP8","ENP9","CCH Naucalpan","CCH Vallejo","CCH Azcapotzalco","CCH Oriente","CCH Sur","Otro"};
+        EscuelaA.setAdapter(new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, vocacional));
+        //spinner carreras
 
-        EscuelaI = (Spinner) findViewById(R.id.EIngresar);
-        String[] superior={"Escuela a Igresar","UPIBI","UPIIZ Campus Zacatecas",
+
+        DatabaseReference recupera = FirebaseDatabase.getInstance().getReference(getString(R.string.nodoCarrera));
+        recupera.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                EscuelaI = (Spinner) findViewById(R.id.EIngresar);
+                ArrayList<String> sup=new ArrayList<>();
+                for(DataSnapshot datasnapshot: dataSnapshot.getChildren())
+                {
+                    Carrera car=datasnapshot.getValue(Carrera.class);
+                    String carre=car.getCarrera();
+                    sup.add(carre);
+                }
+
+                String[] superior= new String[sup.size()];
+                for(int i=0;i<sup.size();i++) {
+                    superior[i]=sup.get(i);
+                }
+                EscuelaI.setAdapter(new ArrayAdapter<String>(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item, superior));
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //String[] superior=sup;
+                /*{"Escuela a Igresar","UPIBI","UPIIZ Campus Zacatecas",
                 "UPIITA","ENCB","UPIIG Campus Guanajuato","ESIA Unidad Zacatenco",
                 "ESIME Unidad Zacatenco","ESIME Unidad Ticomán","ESIME Unidad Culhuacán",
                 "UPIICSA","ESIQIE","ESIME Unidad Azcapotzalco","UPIIH Campus Hidalgo",
                 "ESCOM","ESIA Unidad Ticomán","ESFM","ESIT","ESIA Unidad Tecamachalco",
                 "CICS Unidad Milpa Alta","CICS Unidad Santo Tomás","ESEO","ENMyH",
-                "ESM","ESCA Unidad Santo Tomás","ESCA Unidad Tepepan","ESE","EST"};
-        EscuelaI.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, superior));
+                "ESM","ESCA Unidad Santo Tomás","ESCA Unidad Tepepan","ESE","EST"};*/
+
 
         NickField = (EditText) findViewById(R.id.Nick);
         CorField = (EditText) findViewById(R.id.Cor);
